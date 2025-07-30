cb6d63669be8eacd5c69b3498bd6284a3e5e8ae3
Henry <henrycujcuj42@gmail.com>
2025-07-30 16:16:42 -0600

    06-repeatString completado

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+  let acumuladorCadena = ''
+
+    if (number < 0) {
+        return 'ERROR'
+
+    } else {
+        for (let i = 0; i < number; i++ ) {
+            
+            acumuladorCadena += string
+        }
+    }
+
+    return acumuladorCadena
 
 };
+
+console.log(repeatString('bye', -1))
 
 // Do not edit below this line
 module.exports = repeatString;
