26e7b533176efe3aa946f9757f43386b199b4494
Henry <henrycujcuj42@gmail.com>
2025-07-30 17:33:59 -0600

    07-reverseString completado

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(palabra) {
+    // return palabra.split('').reverse().join('')
+    let palabraInvertida = ''
+    for(let i = palabra.length - 1 ; i >= 0; i--){
+        console.log(palabra[i]);
+        palabraInvertida += palabra[i]
+    }
+    return palabraInvertida
+}
 
-};
+console.log(reverseString('Hola'));
 
 // Do not edit below this line
 module.exports = reverseString;
