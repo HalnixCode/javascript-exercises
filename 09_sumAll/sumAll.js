4a742f6d270f1159544e98fc5d808e83afd52265
Henry <henrycujcuj42@gmail.com>
2025-07-31 13:13:38 -0600

    09-sumAll completado

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    const esNumeroValido = n => typeof n === 'number' && Number.isInteger(n) && n >= 0;
+    
+    if (!esNumeroValido(num1) || !esNumeroValido(num2)) {
+        return 'ERROR'
+    }   
 
+    const menor = Math.min(num1, num2)
+    const mayor = Math.max(num1, num2)
+    
+    let sumaTotal = 0
+    for (let i = menor; i <= mayor; i++) {
+        sumaTotal += i
+    }
+    return sumaTotal
 };
 
+console.log(sumAll(123, 1));
 // Do not edit below this line
 module.exports = sumAll;
