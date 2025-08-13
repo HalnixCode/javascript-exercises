e795af64c46ded002aef23caa0440b50b822221f
Henry <henrycujcuj42@gmail.com>
2025-08-13 12:36:18 -0600

    13 palindromes completado

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (texto) {
+    const textolimpio = texto
+        .toLowerCase()
+        .replace(/[ .,!]/g, '')
 
+    console.log(textolimpio)   
+    
+    const textoRevertido = texto
+        .toLowerCase()
+        .split('')
+        .reverse()
+        .join()
+        .replace(/[ .,!]/g, '')
+
+    console.log(textoRevertido)
+     
+    return textolimpio === textoRevertido
 };
-
+console.log(palindromes('oso'))
 // Do not edit below this line
 module.exports = palindromes;
