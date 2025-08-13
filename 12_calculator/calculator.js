0448687f41d10471ca4628bea273c69264c208f0
Henry <henrycujcuj42@gmail.com>
2025-08-13 11:18:25 -0600

    12 calculator completado

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((suma, item) => suma + item, 0)
+  // let suma = 0
+  // for (let i = 0; i < arr.length; i++) {
+  //   console.log(arr[i])
+  //   suma += arr[i]
+  // }
+  // console.log(suma)
+  // return suma
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, item) => acc * item) 
+  // let acumulador = 1
+  // for (let i = 0; i < arr.length; i++) {
+  //   console.log(arr[i])
+  //   acumulador = acumulador * arr[i]
+  // }
+  // console.log(acumulador)
+  // return acumulador
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
+  // return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let acumulador = 1
+	for (let i = num; i >= 1;  i--) {
+    acumulador = acumulador * i
+  } 
+  return acumulador 
 };
 
 // Do not edit below this line
